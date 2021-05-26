a2937b2c7e308e2af1b0688bdf3651572dd8d5b4
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2021-05-27 01:52:39 +0800

    Update publisher_add.php

--- rock/admin/publisher_add.php
+++ rock/admin/publisher_add.php
@@ -5,9 +5,7 @@
 
     if (isset($_POST['place']) && isset($_POST['rating']))
     {
-        $place = $_POST['place'];
-        $rating = $_POST['rating'];
-        $query = "INSERT INTO `publisher`(`Publisher_ID`, `PlaceOfOrigin`, `AlbumsPublished`, `Rating`) VALUES (NULL, '".  $_POST['place'] ."', '0', '". $_POST['rating'] ."')";
+        $query = "INSERT INTO `publisher`(`Publisher_ID`, `PublisherName`, `PlaceOfOrigin`, `AlbumsPublished`, `Rating`) VALUES (NULL, '" . $_POST['publisher'] ."','".  $_POST['place'] ."', '".  $_POST['albums'] ."', '". $_POST['rating'] ."')";
 
         if (!mysqli_query($conn, $query))
         {
@@ -29,7 +27,9 @@
         <h2>Add a publisher</h2>
         
         <form method="POST" action="publisher_add.php">
+            Publisher Name: <input type="text" id="place" name="publisher"><br />
             Place of Origin: <input type="text" id="place" name="place"><br />
+            Albums Published: <input type="number" id="place" name="albums"><br />
             Rating: <input type="number" id="place" name="rating"> <br />
             <br />            
             <button type="submit" name="submitBtn" value="Submit">Submit</button>
